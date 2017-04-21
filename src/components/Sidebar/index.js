0af7aac41925dacd867057ffc2b90017fc6f2ebb
Brandon Liu <bliu7654@gmail.com>
2017-04-20 18:53:17 -0700

    Add settings and also touch up styles

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -7,11 +7,13 @@ import UserName from './userName';
 import ProfilePicture from './profilePicture';
 import Checkin from './checkin';
 import Leaderboard from './leaderboard';
+import Settings from './settings';
 
 class SideBar extends React.Component {
     render () {
         return(
             <div className="sidebar">
+                <Settings pic="http://www.freeiconspng.com/uploads/settings-icon-26.png"/>
                 <ProfilePicture pic="https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAeeAAAAJDU1OGQ3NGZhLTY1MjgtNGYzNS1iMDc5LTliODhlMzg1NjJmMw.jpg"/>
                 <Organization org="UCLA ACM" />
                 <UserName userName="Vic Yeh" />
