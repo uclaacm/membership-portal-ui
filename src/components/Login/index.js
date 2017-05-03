cf343eb5f5a70ced5520d232f5c4de1f36a18c61
Brandon Liu <bliu7654@gmail.com>
2017-05-03 08:10:04 -0700

    fixing styles, incomplete

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -8,7 +8,13 @@ class LoginComponent extends React.Component {
     render () {
         return(
             <div className="login">
-                <LoginSidebar/>
+                <LoginSidebar className="login-tile"/>
+                <p className="login-tile">There is other content and images that go here
+                    There's also an issue with making the div the full height :( @kevin pls help
+                    <br></br>
+                    Essentially 100vh works but it kind of breaks other things when scrolling.
+
+                </p>
             </div>
         )
     }
