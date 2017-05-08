7d9805556c5bde50b41aa11197a86d880633eef9
Brandon Liu <bliu7654@gmail.com>
2017-05-08 16:51:35 -0700

    Add fontawesome styling. Add navigation/events tile stuff

--- src/components/Sidebar/navigation.js
+++ src/components/Sidebar/navigation.js
@@ -1,11 +1,14 @@
-import React, { PropTypes } from 'react'
+import React, { PropTypes } from 'react';
+import NavigationTile from './navigationTile';
 
 class Navigation extends React.Component {
     render () {
         return(
-            <div>
-                Events Profile <br></br>
-                Leaderboard Resources
+            <div className="navigation">
+                <NavigationTile icon="fa-calendar fa-2x" text="Events"/>
+                <NavigationTile icon="fa-user fa-2x" text="Profile"/>
+                <NavigationTile icon="fa-list fa-2x" text="Leaderboard"/>
+                <NavigationTile icon="fa-file fa-2x" text="Resources"/>
             </div>
         );
     }
