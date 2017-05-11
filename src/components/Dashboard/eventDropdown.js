b698f604d5f29e665a3a3fb37be2f0ce33ea4e96
Brandon Liu <bliu7654@gmail.com>
2017-05-10 23:57:43 -0700

    Add event dropdown

--- src/components/Dashboard/eventDropdown.js
+++ src/components/Dashboard/eventDropdown.js
@@ -1,10 +1,25 @@
 import React, { PropTypes } from 'react'
+import Button from 'components/Button/index'
 
 class EventDropdown extends React.Component {
     render () {
         return(
             <div className="event-dropdown">
-                
+                <div className="event-top">
+                    <div className="dropdown-left">
+                        <span className="Title-2Secondary">Points</span>
+                        <span className="event-points-val Display-2Secondary">5</span>
+                        <Button className="event-button" style="event-button" icon="fa-facebook-square" text="Event Page"/>
+                        <Button className="event-button" style="event-button" icon="fa-file" text="Resources"/>
+
+                    </div>
+                    <div className="dropdown-right">
+                        <p className="Subheader-2Secondary">Your job-hunt should not end when you get all your dream job offers. There is one last step that many people (especially college students) always forget about: negotiation. This is unfortunate because negotiation takes very little work, but can have a very large impact on your compensation. By explaining how to negotiate, we hope to dispel the fear around negotiation and teach you how to maximize your compensation package.</p>
+                    </div>
+                </div>
+                <div className="minimize">
+                    <i className="fa fa-chevron-up fa-3x minimize-icon" aria-hidden="true"></i>
+                </div>
             </div>
         );
     }
--- src/components/Dashboard/eventsDashboard.js
+++ src/components/Dashboard/eventsDashboard.js
@@ -6,26 +6,15 @@ import EventDay from './eventDay'
 class EventsDashboard extends React.Component {
     render () {
         const fullEventsList = this.props.events;
-        // console.log(this.props.events.length);
-        // for(let i = 0; i < this.props.events.length; i++) {
-        //     console.log(this.props.events[i]);
-        // }
-        // console.log(this.props.events[0]);
-
         return(
             <div className="events-dashboard">
                 <EarlierEventsIcon/>
                 <Button className="checkin-button" style="blue" text="Check In"/>
-                {/*Map the entire json object... will have to update this later.*/}
                 {
                     fullEventsList.map(function(singleDay, i) {
                         return <EventDay singleDayEventsList={singleDay} key={i}/>
                     })
                 }
-
-
-                {/*<EventDay date="Wednesday, April 19th"/>
-                <EventDay date="Thursday, April 20th"/>*/}
             </div>
         );
     }
