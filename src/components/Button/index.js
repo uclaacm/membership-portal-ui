a863ac044521d2cced91f99821f11f01738a652a
Brandon Liu <bliu7654@gmail.com>
2017-05-22 04:52:37 -0700

    Button/index.js: Fix button styling w/ icon

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -6,21 +6,10 @@ class Button extends React.Component {
     render () {
         return(
             <div className={"button-component " + this.props.className}>
-                <button className={"generic-button " + this.props.style}><i className={"fa " + this.props.icon + " button-icon"} aria-hidden="true"></i>{this.props.text}</button>
+                <button className={"generic-button " + this.props.style}>{this.props.icon && <i className={"fa " + this.props.icon + " button-icon"} aria-hidden="true"></i>}{this.props.text}</button>
             </div>
         );
     }
 }
 
 export default Button;
-
-
-
-/*var barWidth1 = 2.5*this.props.points + 'px';
-        var barWidth2 = 2.5*(100 - this.props.points) + 'px';
-        return (
-            <div className="points-bar">
-                <p className="pts points-earned" style={{width:barWidth1}}></p>
-                <p className="pts points-left" style={{width:barWidth2}}></p>
-            </div>
-        );*/
