311d76114103e597fa55707a2a34b61b3ff4c02a
Nikhil Kansal <nkansal96@gmail.com>
2017-05-27 16:00:04 -0700

    fixed/updated event container js

--- src/containers/home.js
+++ src/containers/home.js
@@ -42,21 +42,25 @@ const mapStateToProps = (state)=>{
     const e = state.Events;
     const k = e.get('events');
     let eventDays = [];
-    for(let i = 0; i < k.length; i++){
+
+    for(let i = 0; i < k.length; i++) {
         const event = {
-                "img": k[i].cover,
-                "org": k[i].committee,
-                "time": moment(k[i].startDate).format('h:mm a'),
-                "title": k[i].title,
-                "location": k[i].location,
-                "description": k[i].description,
-                "attendancePoints": k[i].attendancePoints
+            cover: k[i].cover,
+            committee: k[i].committee,
+            startDate: moment(k[i].startDate),
+            endDate: moment(k[i].endDate),
+            eventLink: k[i].eventLink,
+            title: k[i].title,
+            location: k[i].location,
+            description: k[i].description,
+            attendancePoints: k[i].attendancePoints
         };
-        if(eventDays.length < 1 || moment(k[i].startDate).format('dddd, MMMM Do') !== eventDays[eventDays.length - 1].date){
+
+        if(eventDays.length < 1 || moment(k[i].startDate).format('dddd, MMMM Do') !== eventDays[eventDays.length - 1].dateStr){
             eventDays.push({
-                "date": moment(k[i].startDate).format('dddd, MMMM Do'),
-                "dateDigit": moment(k[i].startDate).format('DD'),
-                "events": [event]
+                dateStr: moment(k[i].startDate).format('dddd, MMMM Do'),
+                date: moment(k[i].startDate),
+                events: [event]
             });
         } else {
             eventDays[eventDays.length - 1].events.push(event);
