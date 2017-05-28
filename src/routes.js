131b04f4af33ee8bf11f5ced97c4774366fd8051
Nikhil Kansal <nkansal96@gmail.com>
2017-05-27 21:20:15 -0700

    fixed include name case

--- src/routes.js
+++ src/routes.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router-dom';
 
 import App from './components/App';
-import Dashboard from './components/DashBoard/';
+import Dashboard from './components/Dashboard/';
 
 export default (
     <Route path="/" component={App}>
         <Route path="register" component={Register} />
         <Route path="login" component={Login} />
-        <Route path="dashboard" component={DashBoard} />
+        <Route path="dashboard" component={Dashboard} />
 
         <Route path="*" component={NotFoundPage} />
     </Route>
