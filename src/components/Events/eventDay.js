78ebee9fcda1fed871ba27a29ad4ee9854bff6c1
Nikhil Kansal <nkansal96@gmail.com>
2017-05-29 14:57:13 -0700

    fixed js class bug

--- src/components/Events/eventDay.js
+++ src/components/Events/eventDay.js
@@ -4,7 +4,7 @@ import EventCard from './eventCard'
 export default class EventDay extends React.Component {
     render () {
         return(
-            <div className={"event-day" + this.props.admin ? " admin-dashboard" : ""}>
+            <div className={"event-day" + (this.props.admin ? " admin-dashboard" : "")}>
                 <h1 className="Display-2Primary">{this.props.day.dateStr}</h1>
                 { this.props.day.events.map((ev, i) => <EventCard event={ev} key={i} admin={this.props.admin} onClick={this.props.onClick} />) }
             </div>
