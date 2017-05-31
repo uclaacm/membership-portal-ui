dff51f2f82180d4be137ef05dc034c2ed6f20950
Nikhil Kansal <nkansal96@gmail.com>
2017-05-31 11:23:36 -0700

    added logout button to sidebar

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Config from 'config';
+import Button from 'components/Button';
 
 import Points from './points';
 import Username from './username';
@@ -8,9 +9,20 @@ import Organization from './organization';
 import ProfilePicture from './profilePicture';
 
 export default class Sidebar extends React.Component {
+    constructor(props) {
+        super(props);
+        this.signOut = this.signOut.bind(this);
+    }
+
+    signOut() {
+        window.localStorage.clear();
+        window.location.href = "/login";
+    }
+
     render () {
         return (
             <div className="sidebar">
+                <Button className="logout-btn" style="gray collapsed" icon="fa fa-sign-out" onClick={this.signOut} />
                 <div className="sidebar-container">
                     <ProfilePicture pic={this.props.pic}/>
                     <Organization org={Config.organization.shortName} />
