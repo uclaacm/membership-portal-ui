f6e9b3ea60947975a44db93ac9d41df360fd7d09
Brandon Liu <bliu7654@gmail.com>
2017-06-01 13:59:00 -0700

    stash

--- src/components/Events/AdminEvents/event.js
+++ src/components/Events/AdminEvents/event.js
@@ -47,9 +47,46 @@ export default class AdminEventCard extends React.Component {
         const event = this.props.event;
         
         return (
-            <div className="event-tile">
-
+            <div className="admin-event-tile">
+                <div className="top">
+                    <div className="cover" style={{ backgroundImage: 'url('+event.cover+')' }}></div>
+                    <div className="event-header">
+                        <span className="event-title Headline-2Primary">{event.title}</span><br/>
+                        <span className="event-committee Title-2Secondary">{event.committee}</span>
+                    </div>
+                </div>
+                {/*<div className="dropdown">*/}
+                <div className="bottom">
+                    <div className="midcontent">
+                        <div className="description" dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(event.description) }}></div>
+                        <p className="event-link"><a target="_BLANK" href={event.eventLink}>Go to the event page</a></p>
+                    </div>
+                    <div className="subcontent">
+                        <div className="left">
+                            <div className="points">{event.attendancePoints}</div>
+                            <div className="label">Points</div>
+                        </div>
+                        <div className="right">
+                            <p  className="time">{event.empty ? event.startDate : event.startDate.format("h:mm a")} &mdash; {event.empty ? event.endDate : event.endDate.format("h:mm a")}</p>
+                            <p className="location">{event.location}</p>
+                        </div>
+                        <div className="edit-delete-buttons">
+                            <Button onClick={(e) => {
+                                e.stopPropagation();
+                                this.editEvent();
+                            }
+                            } className="edit-event-button" style={"collapsed" + (this.state.editable ? " green" : " blue")} text="" icon="fa-pencil" />
+                            <Button onClick={(e) => {
+                                e.stopPropagation();
+                                this.deleteEvent();
+                            }
+                            } className="delete-event-button" style="red collapsed" text="" icon="fa-trash-o" />
+                        </div>
+                        <div style={{clear: "both"}}></div>
+                    </div>
+                </div>
             </div>
+
         );
 
         /*if(this.props.addEvent) {
