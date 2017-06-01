f2f81d75d82a94f7e2af1d8ff1be92e822192cc8
Kevin Wang <wangkevin448@gmail.com>
2017-06-01 00:09:16 -0700

    registration

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -8,6 +8,7 @@ import {User, FetchUser, UpdateUser, UserUpdateDone} from './user';
 import {Auth, LoginUser, LogoutUser} from './auth';
 import {Events, GetCurrentEvents, PostNewEvent} from './events';
 import {Leaderboard, FetchLeaderboard, InvalidateLeaderboard} from './leaderboard';
+import {Registration, RegisterUser} from './registration';
 
 const history = createHistory();
 const routing = routerMiddleware(history);
@@ -19,6 +20,7 @@ const store = createStore(
     Events,
     User,
     Leaderboard,
+    Registration,
     router: routerReducer,
   }),
   applyMiddleware(routing, thunk)
@@ -26,9 +28,10 @@ const store = createStore(
 
 const Action = {
   TimeGet,
-  LoginUser, LogoutUser, FetchUser, UpdateUser, UserUpdateDone, 
+  LoginUser, LogoutUser, FetchUser, UpdateUser, UserUpdateDone,
   GetCurrentEvents, PostNewEvent,
   FetchLeaderboard, InvalidateLeaderboard,
+  RegisterUser,
 };
 
 export {
