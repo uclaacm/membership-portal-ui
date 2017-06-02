f42ee80a848d0d23338ed9147ca74b3bd6849820
Brandon Liu <bliu7654@gmail.com>
2017-06-02 02:42:22 -0700

    some stuff with momentjs

--- src/components/Events/AdminEvents/adminAddEvent.js
+++ src/components/Events/AdminEvents/adminAddEvent.js
@@ -8,8 +8,30 @@ export default class AdminAddEvent extends React.Component {
         const event = this.props.event;
         const header = (this.props.isEdit? "Update" : "Add") + " Event";
         const timeMask = "99:99";
-        const dateMask = "99/99/99";
+        const dateMask = "99-99-99";
 
+        const startDate = event.startDate ? (event.startDate.format("MM-D-YY")) : "";
+        const endDate = event.endDate ? (event.endDate.format("MM-D-YY")) : "";
+        console.log
+
+        const startTime = event.startDate ? (event.startDate.format("HH:mm")) : "";
+        console.log(startTime);
+
+
+        //2pm returns 14:00
+        var endTime = event.endDate ? (event.endDate.format("h:mm a")) : "";
+        //h:mm gives the hour/min up to 12
+        //a gives am or pm
+
+        console.log(endTime);
+  
+        console.log("Hr", hr);
+        console.log("am?", am);
+
+// // H HH	0..23	Hours (24 hour time)
+// h hh	1..12	Hours (12 hour time used with a A.)
+// k kk	1..24	Hours (24 hour time from 1 to 24)
+// a A	am pm	Post or ante meridiem (Note the one character a p are also considered valid)
 
 
         return (
@@ -22,12 +44,12 @@ export default class AdminAddEvent extends React.Component {
                     <AdminInput val={event.committee}text="Committee" field="committee"/>
                     <AdminInput val={event.attendancePoints}text="Points" field="small"/>
                     <div className="split"></div>
-                    <AdminInput val={""} text="Start Time" mask={timeMask} field="half" />
-                    <AdminInput val={""} text="Start Date" mask={dateMask} field="half" />
-                    <AdminInput val={""} placeholder={"AM"} text="" mask={"aa"} field="small" />
-                    <AdminInput val={""} text="End Time" mask={timeMask} field="half" />
-                    <AdminInput val={""} text="" mask={"aa"} field="small" />
-                    <AdminInput text="End Date" mask={dateMask} field="half"/>
+                    <AdminInput val={""} placeholder={"hh:mm"} text="Start Time" mask={timeMask} field="medium" />
+                    <AdminInput val={""} placeholder={"am"} text="" mask={"aa"} field="small time-of-day" />
+                    <AdminInput val={startDate} placeholder={"mm-dd-yy"} text="Start Date" mask={dateMask} field="medium" />
+                    <AdminInput val={""} placeholder={"hh:mm"} text="End Time" mask={timeMask} field="medium" />
+                    <AdminInput val={""} placeholder={"am"} text="" mask={"aa"} field="small time-of-day" />
+                    <AdminInput val={endDate} placeholder={"mm-dd-yy"} text="End Date" mask={dateMask} field="medium"/>
                     <AdminInput val={event.location}text="Location" field="full"/>
                     <AdminInput val={event.description}text="Description" field="full"/>
                     <div className="buttons">
