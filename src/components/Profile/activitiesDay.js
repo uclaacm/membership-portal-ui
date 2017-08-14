6cd393a6a2dd59b660f023228b7fe6e5a0d44aee
Nikhil Kansal <nikkansa@cisco.com>
2017-08-14 11:30:41 -0700

    code cleanup, bug fix, added activity view

---
+++ src/components/Profile/activitiesDay.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Activity from './activity';
+export default class Activities extends React.Component {
+  render() {
+    return (
+      <div className="day">
+        <div className="day-label">{this.props.day.date.format('DD')}</div>
+        { this.props.day.activities.map((activity, i) => <Activity key={'activity'+i} activity={activity} />) }
+      </div>
+    );
+  }
+}
--- src/components/Profile/profile.js
+++ src/components/Profile/profile.js
@@ -4,230 +4,235 @@ import Button from 'components/Button';
 import OverlayPopup from 'components/OverlayPopup';
 import BannerMessage from 'components/BannerMessage';
 
+import Activities from './activities';
 import YearSelector from './yearSelector';
 import MobileProfile from './mobileProfile';
 
 export default class Profile extends React.Component {
-    constructor(props) {
-        super(props);
+	constructor(props) {
+		super(props);
 
-        this.state = {
-            profile: Object.assign({}, this.props.profile),
-            originalProfile: Object.assign({}, this.props.profile),
-            password: '',
-            passwordNew: '',
-            passwordConf: '',
-            showChangePassword: false
-        };
+		this.state = {
+			profile: Object.assign({}, this.props.profile),
+			originalProfile: Object.assign({}, this.props.profile),
+			password: '',
+			passwordNew: '',
+			passwordConf: '',
+			showChangePassword: false
+		};
 
-        this.inputs = {};
-        this.saveProfile = this.saveProfile.bind(this);
-        this.handleUpdate = this.handleUpdate.bind(this);
-        this.registerInput = this.registerInput.bind(this);
-        this.resizeTextAreas = this.resizeTextAreas.bind(this);
-        this.hideChangePassword = this.hideChangePassword.bind(this);
-        this.showChangePassword = this.showChangePassword.bind(this);
-        this.submitChangePassword = this.submitChangePassword.bind(this);
-    }
+		this.inputs = {};
+		this.saveProfile = this.saveProfile.bind(this);
+		this.handleUpdate = this.handleUpdate.bind(this);
+		this.registerInput = this.registerInput.bind(this);
+		this.resizeTextAreas = this.resizeTextAreas.bind(this);
+		this.hideChangePassword = this.hideChangePassword.bind(this);
+		this.showChangePassword = this.showChangePassword.bind(this);
+		this.submitChangePassword = this.submitChangePassword.bind(this);
+	}
 
-    registerInput(input) {
-        if (input && input.name)
-            this.inputs[input.name] = input;
-    }
+	registerInput(input) {
+		if (input && input.name)
+			this.inputs[input.name] = input;
+	}
 
-    resizeTextAreas() {
-        for (let input in this.inputs) {
-            this.inputs[input].style.height = "auto";
-            this.inputs[input].style.height = (this.inputs[input].scrollHeight) + "px";
-        }
-    }
+	resizeTextAreas() {
+		for (let input in this.inputs) {
+			this.inputs[input].style.height = "auto";
+			this.inputs[input].style.height = (this.inputs[input].scrollHeight) + "px";
+		}
+	}
 
-    handleUpdate(e) {
-        this.resizeTextAreas();
-        let name = e.target.name;
-        let value = e.target.value;
-        this.setState(prev => {
-            let newState = Object.assign({}, prev);
-            newState.profile[name] = value;
-            return newState;
-        });
-    }
+	handleUpdate(e) {
+		this.resizeTextAreas();
+		let name = e.target.name;
+		let value = e.target.value;
+		this.setState(prev => {
+			let newState = Object.assign({}, prev);
+			newState.profile[name] = value;
+			return newState;
+		});
+	}
 
-    hideChangePassword(e) {
-        this.setState(prev => Object.assign({}, prev, { showChangePassword: false }));
-    }
+	hideChangePassword(e) {
+		this.setState(prev => Object.assign({}, prev, { showChangePassword: false }));
+	}
 
-    showChangePassword(e) {
-        this.setState(prev => Object.assign({}, prev, { showChangePassword: true }));
-    }
+	showChangePassword(e) {
+		this.setState(prev => Object.assign({}, prev, { showChangePassword: true }));
+	}
 
-    submitChangePassword(e) {
-        e.preventDefault();
-        let newProfile = {
-            password: this.state.password,
-            newPassword: this.state.passwordNew,
-            confPassword: this.state.passwordConf,
-        };
-        
-        this.props.saveChanges(newProfile);
-    }
+	submitChangePassword(e) {
+		e.preventDefault();
+		let newProfile = {
+			password: this.state.password,
+			newPassword: this.state.passwordNew,
+			confPassword: this.state.passwordConf,
+		};
+		
+		this.props.saveChanges(newProfile);
+	}
 
-    profileUpdated() {
-        return parseInt(this.state.profile.year) !== parseInt(this.state.originalProfile.year) ||
-               this.state.profile.name !== this.state.originalProfile.name ||
-               this.state.profile.major !== this.state.originalProfile.major;
-    }
+	profileUpdated() {
+		return parseInt(this.state.profile.year) !== parseInt(this.state.originalProfile.year) ||
+			   this.state.profile.name !== this.state.originalProfile.name ||
+			   this.state.profile.major !== this.state.originalProfile.major;
+	}
 
-    saveProfile(e) {
-        let nameArray = this.state.profile.name.trim().replace(/\s{2,}/g, " ").split(' ');
-        if (nameArray.length !== 2) {
-            this.refs.banner.showBanner("Please enter a valid first and last name", false);
-            return;
-        }
+	saveProfile(e) {
+		let nameArray = this.state.profile.name.trim().replace(/\s{2,}/g, " ").split(' ');
+		if (nameArray.length !== 2) {
+			this.refs.banner.showBanner("Please enter a valid first and last name", false);
+			return;
+		}
 
-        if (parseInt(this.state.profile.year) === NaN) {
-            this.refs.banner.showBanner("Please select a valid year", false);
-            return;
-        }
+		if (parseInt(this.state.profile.year) === NaN) {
+			this.refs.banner.showBanner("Please select a valid year", false);
+			return;
+		}
 
-        let firstName = nameArray[0].replace(/\n/g, '');
-        let lastName = nameArray[1].replace(/\n/g, '');
-        let year = this.state.profile.year;
-        let major = this.state.profile.major.replace(/\n/g, '');
+		let firstName = nameArray[0].replace(/\n/g, '');
+		let lastName = nameArray[1].replace(/\n/g, '');
+		let year = this.state.profile.year;
+		let major = this.state.profile.major.replace(/\n/g, '');
 
-        this.props.saveChanges({ firstName, lastName, year, major });
-    }
+		this.props.saveChanges({ firstName, lastName, year, major });
+	}
 
-    componentWillMount() {
-        this.resizeTextAreas();
-    }
+	componentWillMount() {
+		this.resizeTextAreas();
+	}
 
-    componentDidMount() {
-        window.addEventListener("resize", this.resizeTextAreas);
-    }
+	componentDidMount() {
+		window.addEventListener("resize", this.resizeTextAreas);
+	}
 
-    componentWillUnmount() {
-        window.removeEventListener("resize", this.resizeTextAreas);
-    }
+	componentWillUnmount() {
+		window.removeEventListener("resize", this.resizeTextAreas);
+	}
 
-    componentWillReceiveProps(nextProps) {
-        this.setState({
-            profile: Object.assign({}, nextProps.profile),
-            originalProfile: Object.assign({}, nextProps.profile),
-            password: nextProps.updateSuccess ? '' : this.state.password,
-            passwordNew: nextProps.updateSuccess ? '' : this.state.passwordNew,
-            passwordConf: nextProps.updateSuccess ? '' : this.state.passwordConf,
-            showChangePassword: this.state.showChangePassword && !nextProps.updateSuccess
-        });
+	componentWillReceiveProps(nextProps) {
+		this.setState({
+			profile: Object.assign({}, nextProps.profile),
+			originalProfile: Object.assign({}, nextProps.profile),
+			password: nextProps.updateSuccess ? '' : this.state.password,
+			passwordNew: nextProps.updateSuccess ? '' : this.state.passwordNew,
+			passwordConf: nextProps.updateSuccess ? '' : this.state.passwordConf,
+			showChangePassword: this.state.showChangePassword && !nextProps.updateSuccess
+		});
 
-        this.resizeTextAreas();
-    }
+		this.resizeTextAreas();
+	}
 
-    render() {
-        if (this.props.error)
-            return <div className="profile-wrapper"><h1>{this.props.error}</h1></div>;
+	render() {
+		if (this.props.error)
+			return <div className="profile-wrapper"><h1>{this.props.error}</h1></div>;
 
-        const { currLevel, nextLevel } = Utils.getLevel(this.props.profile.points);
-        return (
-            <div>
-                <BannerMessage
-                    ref="banner"
-                    showing={this.props.updated}
-                    success={this.props.updateSuccess}
-                    message={this.props.updateSuccess ? "Profile successfully updated." : this.props.updateError} />
-                <OverlayPopup
-                    onCancel={ this.hideChangePassword }
-                    onSubmit={ this.submitChangePassword }
-                    showing={ this.state.showChangePassword }
-                    title="Change Password"
-                    submitText="Update"
-                    cancelText="Cancel">
-                    <form onSubmit={this.submitChangePassword}>
-                        <input type="password" placeholder="Old password..." onChange={(e)=>{let v = e.target.value;this.setState((prev)=>{
-                            return Object.assign({}, prev, {password: v});
-                          });}}/><br />
-                        <input type="password" placeholder="New password..." onChange={(e)=>{let v = e.target.value; this.setState((prev)=>{
-                            return Object.assign({}, prev, {passwordNew: v});
-                          });}}/><br />
-                        <input type="password" placeholder="Confirm new password..." onChange={(e)=>{let v = e.target.value;this.setState((prev)=>{
-                            return Object.assign({}, prev, {passwordConf: v});
-                          });}}/><br />
-                        { this.props.checkInError ? <span className="CaptionSecondary error">{ this.props.checkInError }</span> : <span className="CaptionSecondary error">&nbsp;</span> }
-                    </form>
-                </OverlayPopup>
-                {/*<MobileProfile profile={this.props.profile} />*/}
-                <div className="profile-wrapper">
-                    <div className="form-elem">
-                        <p className="SubheaderSecondary">Hello,</p>
-                        <textarea
-                            rows="1"
-                            type="text"
-                            name="name"
-                            className="Display-2Primary"
-                            ref={this.registerInput}
-                            onChange={this.handleUpdate}
-                            value={this.state.profile.name} name="name" />
-                    </div>
-                    <div className="form-elem">
-                        <p className="SubheaderSecondary">You are a</p>
-                        <YearSelector
-                            target="year"
-                            value={this.state.profile.year}
-                            onChange={this.handleUpdate} />
-                    </div>
-                    <div className="form-elem">
-                        <p className="SubheaderSecondary">majoring in</p>
-                        <textarea
-                            rows="1"
-                            name="major"
-                            type="text"
-                            className="Display-2Primary"
-                            ref={this.registerInput}
-                            value={this.state.profile.major} name="major"
-                            onChange={this.handleUpdate} />
-                    </div>
-                    <div className="form-elem">
-                        <Button
-                            className="profile-action-button"
-                            style={ this.profileUpdated() ? "green" : "disabled" }
-                            onClick= { this.saveProfile }
-                            text="Save" />
-                        <a href="/profile" className="no-style">
-                            <Button
-                                className="profile-action-button"
-                                style={ this.profileUpdated() ? "red" : "disabled" }
-                                text="Discard" />
-                        </a>
-                    </div>
+		const { currLevel, nextLevel } = Utils.getLevel(this.props.profile.points);
+		return (
+			<div>
+				<BannerMessage
+					ref="banner"
+					showing={this.props.updated}
+					success={this.props.updateSuccess}
+					message={this.props.updateSuccess ? "Profile successfully updated." : this.props.updateError} />
+				<OverlayPopup
+					onCancel={ this.hideChangePassword }
+					onSubmit={ this.submitChangePassword }
+					showing={ this.state.showChangePassword }
+					title="Change Password"
+					submitText="Update"
+					cancelText="Cancel">
+					<form onSubmit={this.submitChangePassword}>
+						<input type="password" placeholder="Old password..." onChange={(e)=>{let v = e.target.value;this.setState((prev)=>{
+							return Object.assign({}, prev, {password: v});
+						  });}}/><br />
+						<input type="password" placeholder="New password..." onChange={(e)=>{let v = e.target.value; this.setState((prev)=>{
+							return Object.assign({}, prev, {passwordNew: v});
+						  });}}/><br />
+						<input type="password" placeholder="Confirm new password..." onChange={(e)=>{let v = e.target.value;this.setState((prev)=>{
+							return Object.assign({}, prev, {passwordConf: v});
+						  });}}/><br />
+						{ this.props.checkInError ? <span className="CaptionSecondary error">{ this.props.checkInError }</span> : <span className="CaptionSecondary error">&nbsp;</span> }
+					</form>
+				</OverlayPopup>
+				{/*<MobileProfile profile={this.props.profile} />*/}
+				<div className="profile-wrapper">
+					<div className="form-elem">
+						<p className="SubheaderSecondary">Hello,</p>
+						<textarea
+							rows="1"
+							type="text"
+							name="name"
+							className="Display-2Primary"
+							ref={this.registerInput}
+							onChange={this.handleUpdate}
+							value={this.state.profile.name} name="name" />
+					</div>
+					<div className="form-elem">
+						<p className="SubheaderSecondary">You are a</p>
+						<YearSelector
+							target="year"
+							value={this.state.profile.year}
+							onChange={this.handleUpdate} />
+					</div>
+					<div className="form-elem">
+						<p className="SubheaderSecondary">majoring in</p>
+						<textarea
+							rows="1"
+							name="major"
+							type="text"
+							className="Display-2Primary"
+							ref={this.registerInput}
+							value={this.state.profile.major} name="major"
+							onChange={this.handleUpdate} />
+					</div>
+					<div className="form-elem">
+						<Button
+							className="profile-action-button"
+							style={ this.profileUpdated() ? "green" : "disabled" }
+							onClick= { this.saveProfile }
+							text="Save" />
+						<a href="/profile" className="no-style">
+							<Button
+								className="profile-action-button"
+								style={ this.profileUpdated() ? "red" : "disabled" }
+								text="Discard" />
+						</a>
+					</div>
 
-                    <div className="divider"></div>
+					<div className="divider"></div>
 
-                    <div className="form-elem">
-                        <p className="SubheaderSecondary">Your current rank is</p>
-                        <span className="Display-2Primary">{currLevel.rank}</span>
-                    </div>
-                    <div className="form-elem">
-                        <p className="SubheaderSecondary">You have <span className="Subheader-2Primary">{nextLevel.startsAt - this.props.profile.points}</span> more point(s) until you become a</p>
-                        <span className="Display-2Primary">{nextLevel.rank}</span>
-                    </div>
+					<div className="form-elem">
+						<p className="SubheaderSecondary">Your current rank is</p>
+						<span className="Display-2Primary">{currLevel.rank}</span>
+					</div>
+					<div className="form-elem">
+						<p className="SubheaderSecondary">You have <span className="Subheader-2Primary">{nextLevel.startsAt - this.props.profile.points}</span> more point(s) until you become a</p>
+						<span className="Display-2Primary">{nextLevel.rank}</span>
+					</div>
 
-                    <div className="divider"></div>
-                    <div className="form-elem" style={{paddingBottom: '10px'}}>
-                        <Button
-                            className="profile-action-button"
-                            style="blue"
-                            text="Change Password"
-                            onClick={this.showChangePassword} />
-                    </div>
-                    <div className="form-elem">
-                        <Button
-                            className="profile-action-button"
-                            style="red"
-                            text="Sign Out"
-                            onClick={this.props.logout} />
-                    </div>
-                </div>
-            </div>
-        );
-    }
+					<div className="divider"></div>
+					<div className="form-elem" style={{paddingBottom: '10px'}}>
+						<Button
+							className="profile-action-button"
+							style="blue"
+							text="Change Password"
+							onClick={this.showChangePassword} />
+					</div>
+					<div className="form-elem">
+						<Button
+							className="profile-action-button"
+							style="red"
+							text="Sign Out"
+							onClick={this.props.logout} />
+					</div>
+
+					<div className="divider"></div>
+					<h1 className="Display-2Primary">Your Activity</h1>
+					<Activities />
+				</div>
+			</div>
+		);
+	}
 }
