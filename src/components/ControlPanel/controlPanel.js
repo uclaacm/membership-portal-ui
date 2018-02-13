f3372eaa7d52dae106e99b7cdac1cbd2193fae5b
Alexander Chen <alexanderchen@s-169-232-114-102.resnet.ucla.edu>
2018-02-12 20:12:50 -0800

    converted all spaces to tabs

--- src/components/ControlPanel/controlPanel.js
+++ src/components/ControlPanel/controlPanel.js
@@ -3,9 +3,9 @@ import React from 'react';
 export default class ControlPanel extends React.Component {
 	render() {
 		return (
-      <div className="control-panel-wrapper">
-        <h1>Hello</h1>
-      </div>
+			<div className="control-panel-wrapper">
+				<h1>Hello</h1>
+			</div>
 		);
 	}
 }
