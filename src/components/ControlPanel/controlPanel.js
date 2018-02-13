f21a9f68e3d4377ded3a11f6a092b62232f7d7c0
Alexander Chen <alexanderchen@Alexanders-MacBook-Air.local>
2018-02-12 18:25:09 -0800

    added control panel view for admin

---
+++ src/components/ControlPanel/controlPanel.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class ControlPanel extends React.Component {
+	render() {
+		return (
+			<div className="control-panel-wrapper">
+        <h1>Hello</h1>
+      </div>
+		);
+	}
+}
--- src/main.js
+++ src/main.js
@@ -14,6 +14,7 @@ import Events from 'containers/events';
 import Login from 'containers/login';
 import Profile from 'containers/profile';
 import Leaderboard from 'containers/leaderboard';
+import ControlPanel from 'containers/controlPanel';
 import Resources from 'containers/resources';
 import Register from 'containers/register';
 import ResetPassword from 'containers/resetPassword';
@@ -33,6 +34,7 @@ class App extends React.Component {
 							<Route path="/profile" component={requireAuth(Profile)}/>
 							<Route path="/resources" component={requireAuth(Resources)}/>
 							<Route path="/leaderboard" component={requireAuth(Leaderboard)}/>
+							<Route path="/controlpanel" component={requireAuth(ControlPanel)}/>
 							<Redirect to="/events"/>
 						</Switch>
 					</div>
