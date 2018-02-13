d9fd6b836c090f0d27d136f40cfe18b8a9c3322b
Alexander Chen <alexanderchen@Alexanders-MacBook-Air.local>
2018-02-12 18:48:06 -0800

    fixed tabs issue

--- src/components/ControlPanel/controlPanel.js
+++ src/components/ControlPanel/controlPanel.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default class ControlPanel extends React.Component {
 	render() {
 		return (
-			<div className="control-panel-wrapper">
+      <div className="control-panel-wrapper">
         <h1>Hello</h1>
       </div>
 		);
