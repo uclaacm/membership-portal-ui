d5515a2898df9b50430c33fe6ef4ebb45676d3e3
Boyuan He <boyuan_heboyuan@live.com>
2018-02-14 22:57:12 -0800

    added the milestone input

--- src/components/ControlPanel/controlPanel.js
+++ src/components/ControlPanel/controlPanel.js
@@ -1,11 +1,25 @@
 import React from 'react';
-
+import Button from 'components/Button';
 
 export default class ControlPanel extends React.Component {
 	render() {
 		return (
+			
 			<div className="control-panel-wrapper">
-				<h1>hello world</h1>
+				<div className="form-elem">
+					<h1>Create a milestone</h1>
+					<textarea
+						rows="1"
+						type="text"
+						name="name"
+						placeholder="Quarter (e.g. Fall 2017)" />
+				</div>
+				<div className="form-elem">
+					<Button
+						className="profile-action-button"
+						style={ "blue"}
+						text="Create" />
+				</div>
 			</div>
 		);
 	}
