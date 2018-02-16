8e9bb3586726c29389cb54be61c16fb710e10af2
ruchapatki <ruchapatki@gmail.com>
2018-02-16 01:56:54 -0800

    added sign out and delete events buttons

--- src/components/ControlPanel/controlPanel.js
+++ src/components/ControlPanel/controlPanel.js
@@ -3,12 +3,23 @@ import React from 'react';
 import Topbar from 'containers/topbar';
 import Sidebar from 'containers/sidebar';
 import ControlPanel from './controlPanel';
+import Button from 'components/Button';
 
 export default class ControlPanelComponent extends React.Component {
 	render() {
 		return (
 			<div className="control-panel-wrapper">
-				<h1>Hello</h1>
+				<h1>Control Panel</h1>
+				<div className="form-elem">
+						<Button
+							className="signout-action-button"
+							style="blue"
+							text="Sign Out"/>
+						<Button
+							className="deleteevents-action-button"
+							style="red"
+							text="Delete Events"/>
+				</div>
 			</div>
 		);
 	}
