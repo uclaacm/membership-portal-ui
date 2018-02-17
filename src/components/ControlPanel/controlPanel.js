e6cc1212637c711dfc3036550586acb1c4cf9b1b
Alexander Chen <alexanderchen@s-169-232-114-10.resnet.ucla.edu>
2018-02-16 23:53:06 -0800

    added dropdown menu for committee selection in control panel page for admin

--- src/components/ControlPanel/controlPanel.js
+++ src/components/ControlPanel/controlPanel.js
@@ -4,7 +4,19 @@ export default class ControlPanel extends React.Component {
 	render() {
 		return (
 			<div className="control-panel-wrapper">
-				<h1>Hello</h1>
+				<div className="form-elem">
+					<p className="SubheaderSecondary">Committee</p>
+					<select className="Display-2Primary">
+						<option>General</option>
+						<option>AI</option>
+						<option>Board</option>
+						<option>Hack</option>
+						<option>ICPC</option>
+						<option>NetSec</option>
+						<option>Studio</option>
+						<option>W</option>
+					</select>
+				</div>
 			</div>
 		);
 	}
