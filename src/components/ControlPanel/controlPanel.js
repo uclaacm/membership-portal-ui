6e18f7a71fee2ff565f86fa38d04198227ab3eaf
heboyuan <boyuan_heboyuan@live.com>
2018-02-25 20:01:45 -0800

    test

--- src/components/ControlPanel/controlPanel.js
+++ src/components/ControlPanel/controlPanel.js
@@ -5,7 +5,16 @@ export default class ControlPanel extends React.Component {
 	render() {
 		return (
 			<div className="control-panel-wrapper">
-				<h1>hello world</h1>
+				<div className="form-elem">
+					<h1>Create a milestone</h1>
+					<input type="text" name="name" placeholder="Quarter (e.g. Fall 2017)" />
+				</div>
+				<div className="form-elem">
+					<Button
+						className="control-panel-action-button"
+						style={ "blue"}
+						text="Create" />
+				</div>
 			</div>
 		);
 	}
