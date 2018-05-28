48a8ee372efcac1a1bd235e5834395799c161a30
howardwang15 <joy_luck_2000@hotmail.com>
2018-05-27 23:40:08 -0700

    added close button for modal component

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -6,24 +6,31 @@ import Button from '../Button'
 export default class ModalComponent extends React.Component {
 	constructor(props) {
 		super(props);
-		this.setState({
-			display: false
-		});
+		this.state = {
+			display: true
+		}
 	}
 
 	closeModal() {
 		this.setState({
 			display: !this.state.display
 		});
+		console.log("display: " + state.display);
 	}
 
 	render() {
-		return (
-			<div className="modal-container">
-				<h1>{this.props.title}</h1>
-				<p>{this.props.text}</p>
-				<Button style="red" text="close" onClick={this.closeModal}></Button>
-			</div>
-		)
+		//const display = this.state.display;
+		if (this.state.display) {
+			return (
+				<div className="modal-container">{/*{this.state.display ? "modal-container" : ""*/}
+					<h1>{this.props.title}</h1>
+					<p>{this.props.text}</p>
+					<Button style="red" text="close" onClick={this.closeModal.bind(this)}/>
+				</div> 	
+			)
+		}
+		else {
+			return (<div></div>)
+		}
 	}
 }
