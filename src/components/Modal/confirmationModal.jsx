90509ed170bbafb6da4dcfd51f7693594f613cd8
howardwang15 <joy_luck_2000@hotmail.com>
2018-10-20 12:40:42 -0700

    removed inline styles from confirmation modal, deleted index.jsx

--- src/components/Modal/confirmationModal.jsx
+++ src/components/Modal/confirmationModal.jsx
@@ -1,28 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Button from '../Button';
-import './style.scss';
+import Button from 'components/Button';
 
 export default class ConfirmationModal extends React.Component {
   render() {
     const {
-      title, message, submit, cancel, style,
+      title, message, submit, cancel,
     } = this.props;
     return (
       <div className="modal-wrapper">
-        <div className="confirmation-modal-container" style={style || null}>
-          <div style={{ padding: '30px' }}>
+        <div className="confirmation-modal-container">
+          <div className="padding">
             <h1>{title}</h1>
             <br />
             <p>{message}</p>
             <br />
             <br />
             <div className="button-container">
-              <div style={{ paddingRight: '20px' }}>
-                <Button text="Yes" color="green" onClick={() => submit()} />
+              <div className="button-padding">
+                <Button text="Yes" color="green" onClick={submit} />
               </div>
-              <div style={{ paddingLeft: '20px' }}>
-                <Button text="No" color="red" onClick={() => cancel()} />
+              <div className="button-padding">
+                <Button text="No" color="red" onClick={cancel} />
               </div>
             </div>
           </div>
@@ -32,14 +31,9 @@ export default class ConfirmationModal extends React.Component {
   }
 }
 
-ConfirmationModal.defaultProps = {
-  style: undefined,
-};
-
 ConfirmationModal.propTypes = {
   title: PropTypes.string.isRequired,
   message: PropTypes.string.isRequired,
   submit: PropTypes.func.isRequired,
   cancel: PropTypes.func.isRequired,
-  style: PropTypes.shape,
 };
