438ce2d03e6683ad44b650852dfa97d3984e2465
shirlyfang <shirly.is.here@gmail.com>
2018-11-11 12:15:22 -0800

    added in pink background/ red border for passwords less than 10 chars

--- src/components/Register/detailsCard.js
+++ src/components/Register/detailsCard.js
@@ -1,53 +1,79 @@
-import React from 'react'
-import Config from 'config'
-import Button from 'components/Button'
+import React from 'react';
+import Config from 'config';
+import Button from 'components/Button';
 
 export default class DetailsCard extends React.Component {
-	constructor(props) {
-		super(props)
-		this.handleChange = this.handleChange.bind(this);
-	}
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.state = {
+      passwordLength: 1,
+    };
+  }
 
-	handleChange(e) {
-		if (this.props.onChange)
-			this.props.onChange(e.target.name, e.target.value);
-	}
+  handleChange(e) {
+    if (this.props.onChange) {
+      this.props.onChange(e.target.name, e.target.value);
+      const changed = e.target.name;
+      const changedLength = e.target.value.length;
+      if (changed === 'password') {
+        changedLength < 10 ? this.setState({
+          passwordLength: 0,
+					   }) : this.setState({
+          passwordLength: changedLength,
+        });
+      }
+    }
+  }
 
-	render() {
-		return (
-			<div className={"card details-card" + (this.props.profileValid() ? " confirm-details" : "")}>
-				<img src={Config.organization.logo} />
-				<div className="inner">
-					<form onSubmit={this.props.onSubmit} autoComplete="off">
-						<p className="header">Account Details</p>
+  render() {
+    return (
+      <div className={`card details-card${this.props.profileValid() ? ' confirm-details' : ''}`}>
+        <img src={Config.organization.logo} />
+        <div className="inner">
+          <form onSubmit={this.props.onSubmit} autoComplete="off">
+            <p className="header">Account Details</p>
 
-						<div className="email">
-							<p className="text">School Email <span className="info">(@ucla.edu)</span></p>
-							<input type="text" className="input-large" name="email" onChange={this.handleChange}></input>
-						</div>
-						<div className="password">
-							<p className="text">Password <span className="info">(at least 10 characters)</span></p>
-							<input type="password" className="input-large" name="password" onChange={this.handleChange}></input>
-						</div>
-						<div className="align align-input-major">
-							<p className="text">Major <span className="info">(full name of major)</span></p>
-							<input className="input-major" name="major" onChange={this.handleChange}></input>
-						</div>
-						<div className="align">
-							<p className="text">Year <span className="info">(choose one)</span></p>
-							<select className="input-year" name="year" onChange={this.handleChange}>
-								<option value={0}>--</option>
-								<option value={1}>Freshman</option>
-								<option value={2}>Sophomore</option>
-								<option value={3}>Junior</option>
-								<option value={4}>Senior</option>
-								<option value={5}>Post-senior</option>
-							</select>
-						</div><br />
-						<Button className="btn" loading={this.props.disableForm} style={this.props.profileValid() ? "green" : "disabled"} text="Finish" onClick={this.props.onSubmit}/>
-					</form>
-				</div>
-			</div>
-		);
-	}
+            <div className="email">
+              <p className="text">
+School Email
+                <span className="info">(@ucla.edu)</span>
+              </p>
+              <input type="text" className="input-large" name="email" onChange={this.handleChange} />
+            </div>
+            <div className="password">
+              <p className="text">
+Password
+                <span className="info">(at least 10 characters)</span>
+              </p>
+              <input type="password" className={this.state.passwordLength ? 'input-large' : 'input-invalid'} name="password" onChange={this.handleChange} />
+            </div>
+            <div className="align align-input-major">
+              <p className="text">
+Major
+                <span className="info">(full name of major)</span>
+              </p>
+              <input className="input-major" name="major" onChange={this.handleChange} />
+            </div>
+            <div className="align">
+              <p className="text">
+Year
+                <span className="info">(choose one)</span>
+              </p>
+              <select className="input-year" name="year" onChange={this.handleChange}>
+                <option value={0}>--</option>
+                <option value={1}>Freshman</option>
+                <option value={2}>Sophomore</option>
+                <option value={3}>Junior</option>
+                <option value={4}>Senior</option>
+                <option value={5}>Post-senior</option>
+              </select>
+            </div>
+            <br />
+            <Button className="btn" loading={this.props.disableForm} style={this.props.profileValid() ? 'green' : 'disabled'} text="Finish" onClick={this.props.onSubmit} />
+          </form>
+        </div>
+      </div>
+    );
+  }
 }
--- src/components/Register/index.js
+++ src/components/Register/index.js
@@ -3,6 +3,7 @@ import Config from 'config';
 import { NavLink } from 'react-router-dom';
 import BannerMessage from 'components/BannerMessage';
 
+import { pipeline } from 'stream';
 import NameCard from './nameCard';
 import DetailsCard from './detailsCard';
 import SuccessCard from './successCard';
@@ -14,141 +15,152 @@ const PAGE_DETAILS_CARD = Symbol();
 const PAGE_SUCCESS_CARD = Symbol();
 
 export default class RegisterComponent extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			currentPage: PAGE_FB_LOGIN,
-			disableForm: false,
-			profile: {
-				firstName: "",
-				lastName: "",
-				email: "",
-				year: 0,
-				major: "",
-				password: "",
-				profileId: ""
-			}
-		}
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentPage: PAGE_FB_LOGIN,
+      disableForm: false,
+      profile: {
+        firstName: '',
+        lastName: '',
+        email: '',
+        year: 0,
+        major: '',
+        password: '',
+        profileId: '',
+      },
+    };
 
-		this.nameValid = this.nameValid.bind(this);
-		this.profileValid = this.profileValid.bind(this);
-		this.skipFacebookLogin = this.skipFacebookLogin.bind(this);
-		this.handleFacebookLogin = this.handleFacebookLogin.bind(this);
-		this.handleProfileChange = this.handleProfileChange.bind(this);
-		this.handleProfileSubmit = this.handleProfileSubmit.bind(this);
-		this.handleNameEntryComplete = this.handleNameEntryComplete.bind(this);
-		this.renderComponentForPage = this.renderComponentForPage.bind(this);
-	}
+    this.nameValid = this.nameValid.bind(this);
+    this.profileValid = this.profileValid.bind(this);
+    this.skipFacebookLogin = this.skipFacebookLogin.bind(this);
+    this.handleFacebookLogin = this.handleFacebookLogin.bind(this);
+    this.handleProfileChange = this.handleProfileChange.bind(this);
+    this.handleProfileSubmit = this.handleProfileSubmit.bind(this);
+    this.handleNameEntryComplete = this.handleNameEntryComplete.bind(this);
+    this.renderComponentForPage = this.renderComponentForPage.bind(this);
+  }
 
-	renderComponentForPage(page) {
-		switch (page) {
-			case PAGE_FB_LOGIN:
-				return <FacebookLoginCard
-				         facebookCallback={this.handleFacebookLogin}
-								 skipFacebookLogin={this.skipFacebookLogin} />
-			case PAGE_NAME_CARD:
-				return <NameCard
-				         onChange={this.handleProfileChange}
-								 onSubmit={this.handleNameEntryComplete} 
-								 profileValid={this.nameValid} />
-			case PAGE_DETAILS_CARD:
-				return <DetailsCard
-				         profile={this.state.profile}
-				         onChange={this.handleProfileChange}
-				         onSubmit={this.handleProfileSubmit}
-				         disableForm={this.state.disableForm}
-				         profileValid={this.profileValid} />
-			case PAGE_SUCCESS_CARD:
-				return <SuccessCard />
-			default:
-				return null
-		}
-	}
+  renderComponentForPage(page) {
+    switch (page) {
+      case PAGE_FB_LOGIN:
+        return (
+          <FacebookLoginCard
+            facebookCallback={this.handleFacebookLogin}
+            skipFacebookLogin={this.skipFacebookLogin}
+          />
+        );
+      case PAGE_NAME_CARD:
+        return (
+          <NameCard
+            onChange={this.handleProfileChange}
+            onSubmit={this.handleNameEntryComplete}
+            profileValid={this.nameValid}
+          />
+        );
+      case PAGE_DETAILS_CARD:
+        return (
+          <DetailsCard
+            profile={this.state.profile}
+            onChange={this.handleProfileChange}
+            onSubmit={this.handleProfileSubmit}
+            disableForm={this.state.disableForm}
+            profileValid={this.profileValid}
+          />
+        );
+      case PAGE_SUCCESS_CARD:
+        return <SuccessCard />;
+      default:
+        return null;
+    }
+  }
 
-	skipFacebookLogin(e) {
-		e.preventDefault();
-		this.setState(prev => Object.assign({}, prev, { currentPage: PAGE_NAME_CARD }));
-	}
+  skipFacebookLogin(e) {
+    e.preventDefault();
+    this.setState(prev => Object.assign({}, prev, { currentPage: PAGE_NAME_CARD }));
+  }
 
-	handleFacebookLogin(response) {
-		if (!response || !response.name || !response.id)
-			return;
+  handleFacebookLogin(response) {
+    if (!response || !response.name || !response.id) return;
 
-		this.setState({
-			currentPage: PAGE_DETAILS_CARD,
-			profile: {
-				firstName: response.name.split(" ")[0],
-				lastName: response.name.split(" ")[1],
-				profileId: response.id
-			}
-		})
-	}
+    this.setState({
+      currentPage: PAGE_DETAILS_CARD,
+      profile: {
+        firstName: response.name.split(' ')[0],
+        lastName: response.name.split(' ')[1],
+        profileId: response.id,
+      },
+    });
+  }
 
-	handleNameEntryComplete(e) {
-		e.preventDefault();
-		if (!this.state.disableForm) {
-			if (!this.nameValid())
-				return;
-			this.setState(prev => Object.assign({}, prev, { currentPage: PAGE_DETAILS_CARD }));
-		}
-	}
+  handleNameEntryComplete(e) {
+    e.preventDefault();
+    if (!this.state.disableForm) {
+      if (!this.nameValid()) return;
+      this.setState(prev => Object.assign({}, prev, { currentPage: PAGE_DETAILS_CARD }));
+    }
+  }
 
-	handleProfileChange(name, value) {
-		this.setState(prev => {
-			let newState = Object.assign({}, prev);
-			newState.profile[name] = value;
-			return newState;
-		});
-	}
+  handleProfileChange(name, value) {
+    this.setState((prev) => {
+      const newState = Object.assign({}, prev);
+      newState.profile[name] = value;
+      return newState;
+    });
+  }
 
-	handleProfileSubmit(e) {
-		e.preventDefault();
-		if (!this.state.disableForm) {
-			if (!this.profileValid())
-				return;
-			this.setState(prev => Object.assign({}, prev, { disableForm: true }));
-			this.props.createUser(this.state.profile);
-		}
-	}
+  handleProfileSubmit(e) {
+    e.preventDefault();
+    if (!this.state.disableForm) {
+      if (!this.profileValid()) {
+        return;
+      }
+      this.setState(prev => Object.assign({}, prev, { disableForm: true }));
+      this.props.createUser(this.state.profile);
+    }
+  }
 
-	profileValid() {
-		const p = this.state.profile;
-		return p.firstName
+  profileValid() {
+    const p = this.state.profile;
+    return p.firstName
 			&& p.lastName
 			&& p.major
 			&& p.email
 			&& /^.{2,}\@([^\.\@]{1,}\.)*ucla\.edu$/.test(p.email)
 			&& parseInt(p.year) !== NaN && parseInt(p.year) > 0
 			&& p.password && p.password.length >= 10;
-	}
+  }
 
-	nameValid() {
-		return this.state.profile.firstName && this.state.profile.lastName;
-	}
+  nameValid() {
+    return this.state.profile.firstName && this.state.profile.lastName;
+  }
 
-	componentWillReceiveProps(nextProps) {
-		if (nextProps.created && nextProps.createSuccess) {
-			this.setState(prev => {
-				let newState = Object.assign({}, prev);
-				newState.currentPage = PAGE_SUCCESS_CARD;
-				newState.disableForm = false;
-				return newState;
-			});
-		} else {
-			this.setState(prev => Object.assign({}, prev, { disableForm: false }));
-		}
-	}
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.created && nextProps.createSuccess) {
+      this.setState((prev) => {
+        const newState = Object.assign({}, prev);
+        newState.currentPage = PAGE_SUCCESS_CARD;
+        newState.disableForm = false;
+        return newState;
+      });
+    } else {
+      this.setState(prev => Object.assign({}, prev, { disableForm: false }));
+    }
+  }
 
-	render() {
-		return (
-			<div>
-				<BannerMessage showing={this.props.created && !this.props.createSuccess} success={false} message={this.props.createError} />
-				<div className="register-component">
-					<NavLink to="/login" className="no-style Title-2White login-link"><i className="fa fa-chevron-left" aria-hidden="true"></i>&nbsp; Back to Login</NavLink>
-					{ this.renderComponentForPage(this.state.currentPage) }
-					<img src={Config.organization.logoLight} className="corner-logo" />
-				</div>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div>
+        <BannerMessage showing={this.props.created && !this.props.createSuccess} success={false} message={this.props.createError} />
+        <div className="register-component">
+          <NavLink to="/login" className="no-style Title-2White login-link">
+            <i className="fa fa-chevron-left" aria-hidden="true" />
+&nbsp; Back to Login
+          </NavLink>
+          { this.renderComponentForPage(this.state.currentPage) }
+          <img src={Config.organization.logoLight} className="corner-logo" />
+        </div>
+      </div>
+    );
+  }
 }
