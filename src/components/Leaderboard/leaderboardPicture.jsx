6f41a3704c052efb3b55aca6d57d4770d97c0958
jhan25 <justinhan1997@gmail.com>
2019-02-25 12:57:44 -0800

    added leaderboard modal

--- src/components/Leaderboard/leaderboardPicture.jsx
+++ src/components/Leaderboard/leaderboardPicture.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 class LeaderboardPicture extends React.Component {
   render() {
-    const { picture } = this.props;
+    const { picture, firstName, lastName, major, year, onChange } = this.props;
 
     const pict = picture || '/assets/images/unknown.png';
     const profilePictureAsBackground = {
@@ -11,13 +11,22 @@ class LeaderboardPicture extends React.Component {
     };
 
     return (
-      <div className="profile-pic" style={profilePictureAsBackground} />
+      <div
+          className="profile-pic"
+          style={profilePictureAsBackground}
+          onClick={onChange.bind(this, firstName, lastName, picture, major, year)}
+      />
     );
   }
 }
 
 LeaderboardPicture.propTypes = {
   picture: PropTypes.string.isRequired,
+  firstName: PropTypes.string.isRequired,
+  lastName: PropTypes.string.isRequired,
+  major: PropTypes.string.isRequired,
+  year: PropTypes.number.isRequired,
+  onChange: PropTypes.func.isRequired,
 };
 
 export default LeaderboardPicture;
--- src/components/Leaderboard/topUser.jsx
+++ src/components/Leaderboard/topUser.jsx
@@ -7,8 +7,21 @@ export default class TopUser extends React.Component {
 		return (
 			<div className={"top-user" + (this.props.place === 1 ? " top-user-first" : "")}>
 				<div className="rank">{ ["1st", "2nd", "3rd"][this.props.place-1] }</div><br />
-				<img src={this.props.user.picture || "/assets/images/unknown.png"}/><br />
-				<div className="name">{this.props.user.firstName} {this.props.user.lastName}</div><br />
+				<img
+					src={this.props.user.picture || "/assets/images/unknown.png"}
+					onClick={this.props.onChange
+						.bind(this, this.props.user.firstName, this.props.user.lastName, this.props.user.picture, this.props.user.major, this.props.user.year)}
+				/>
+				<br />
+				<div
+					className="name"
+					onClick={this.props.onChange.bind(
+						this, this.props.user.firstName, this.props.user.lastName, this.props.user.picture, this.props.user.major, this.props.user.year
+					)}
+				>
+					{this.props.user.firstName} {this.props.user.lastName}
+				</div>
+				<br />
 				<div className="level">{currLevel.rank}</div><br />
 				<div className="points">{this.props.user.points} points</div>
 			</div>
