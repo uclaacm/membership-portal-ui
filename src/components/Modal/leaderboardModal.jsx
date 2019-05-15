dfa98cf296c6adea414e2faa69593a76e898f89b
Justin Han <justinhan1997@gmail.com>
2019-05-15 15:21:32 -0700

    Added leaderboard modal (#74)
    * Fixed leaderboard pics
    
    * Changed file extensions
    
    * added leaderboard modal
    
    * Fixed leaderboard pics
    
    * Changed file extensions
    
    * test jenkins
    
    * added topUser modal
    
    * moved array construction in constructor

---
+++ src/components/Modal/leaderboardModal.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Button from '../../components/Button';
+
+export default class LeaderboardModal extends React.Component {
+    constructor() {
+        super();
+        this.yearMap = ["Freshman", "Sophomore", "Junior", "Senior"];
+    }
+
+    render() {
+        const {
+            firstName, lastName, picture, major, year, opened, onChange
+        } = this.props;
+        const gradeLevel = this.yearMap[year-1];
+
+        return opened ? (
+        <div className="modal-wrapper">
+            <div className="leaderboard-modal-container">
+                <div className="padding">
+                    <h1>User Info</h1>
+                    <br />
+                    <br />
+                    <div className="content-container">
+                        <div className="image-container">
+                            <img src={picture || "/assets/images/unknown.png"} />
+                        </div>
+                        <div className="text-container">
+                            <h3>Name:  {firstName} {lastName}</h3>
+                            <h3>Major: {major}</h3>
+                            <h3>Year:  {gradeLevel}</h3>
+                        </div>
+                    </div>
+                    <br />
+                    <br />
+                    <br />
+                    <div className="button-container">
+                    <Button text="Close" color="red" onClick={onChange} />
+                    </div>
+                </div>
+            </div>
+        </div>
+        ) : null;
+    }
+}
+
+LeaderboardModal.propTypes = {
+  firstName: PropTypes.string.isRequired,
+  lastName: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  major: PropTypes.string.isRequired,
+  opened: PropTypes.bool.isRequired,
+  onChange: PropTypes.func.isRequired,
+};
