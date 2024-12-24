3db47cfa0dd5b4e35dcdb692944d609ec4e8a2f9
Snigdha Kansal <snigdha0206@g.ucla.edu>
2024-12-24 15:10:20 -0800

    fix org icon

--- src/components/Sidebar/navigation.jsx
+++ src/components/Sidebar/navigation.jsx
@@ -1,13 +1,15 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
-import NavigationTile from "./navigationTile";
+import { NavLink } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import React from 'react';
+import NavigationTile from './navigationTile';
 
 export default class Navigation extends React.Component {
   render() {
+    const { isAdmin } = this.props;
     return (
       <div>
         {
-          /* USER NAVIGATION */ !this.props.isAdmin && (
+          /* USER NAVIGATION */ !isAdmin && (
             <div className="navigation">
               <NavLink to="/events" activeClassName="selected">
                 <NavigationTile icon="fa-calendar fa-2x" text="Events" />
@@ -22,10 +24,10 @@ export default class Navigation extends React.Component {
                 <NavigationTile icon="fa-file fa-2x" text="Resources" />
               </NavLink>
             </div>
-          )
+      )
         }
         {
-          /* ADMIN NAVIGATION */ this.props.isAdmin && (
+          /* ADMIN NAVIGATION */ isAdmin && (
             <div className="navigation">
               <NavLink to="/events" activeClassName="selected">
                 <NavigationTile icon="fa-calendar fa-2x" text="Events" />
@@ -34,17 +36,21 @@ export default class Navigation extends React.Component {
                 <NavigationTile icon="fa-users fa-2x" text="Members" />
               </NavLink>
               <NavLink to="/resources" activeClassName="selected">
-                {" "}
-                <NavigationTile icon="fa-building-o fa-2x" text="Organization" />
+                {' '}
+                <NavigationTile icon="fa-building fa-2x" text="Organization" />
               </NavLink>
               <NavLink to="/controlpanel" activeClassName="selected">
-                {" "}
+                {' '}
                 <NavigationTile icon="fa-gamepad fa-2x" text="Control Panel" />
               </NavLink>
             </div>
-          )
+      )
         }
       </div>
     );
   }
 }
+
+Navigation.propTypes = {
+  isAdmin: PropTypes.bool.isRequired, // Ensure 'isAdmin' is a required boolean
+};
--- src/components/Topbar/index.jsx
+++ src/components/Topbar/index.jsx
@@ -1,15 +1,16 @@
-import React from "react";
-import { connect } from "react-redux";
-import { NavLink } from "react-router-dom";
-
-import NavigationItem from "./navigationItem";
+import { NavLink } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import React from 'react';
+import NavigationItem from './navigationItem';
 
 export default class Topbar extends React.Component {
   render() {
+    const { isAdmin } = this.props;
+
     return (
       <div className="topbar">
         {
-          /* USER NAVIGATION */ !this.props.isAdmin && (
+          /* USER NAVIGATION */ !isAdmin && (
             <div className="topbar-container">
               <NavLink to="/events" activeClassName="selected">
                 <NavigationItem icon="fa-calendar" text="Events" />
@@ -21,15 +22,15 @@ export default class Topbar extends React.Component {
                 <NavigationItem icon="fa-list" text="Leaderboard" />
               </NavLink>
               <NavLink to="/resources" activeClassName="selected">
-                {" "}
+                {' '}
                 <NavigationItem icon="fa-file" text="Resources" />
               </NavLink>
             </div>
-          )
+      )
         }
 
         {
-          /* ADMIN NAVIGATION */ this.props.isAdmin && (
+          /* ADMIN NAVIGATION */ isAdmin && (
             <div className="topbar-container">
               <NavLink to="/events" activeClassName="selected">
                 <NavigationItem icon="fa-calendar" text="Events" />
@@ -38,17 +39,21 @@ export default class Topbar extends React.Component {
                 <NavigationItem icon="fa-list" text="Members" />
               </NavLink>
               <NavLink to="/resources" activeClassName="selected">
-                {" "}
-                <NavigationItem icon="fa-building-o" text="Organization" />
+                {' '}
+                <NavigationItem icon="fa-building" text="Organization" />
               </NavLink>
               <NavLink to="/controlpanel" activeClassName="selected">
-                {" "}
+                {' '}
                 <NavigationItem icon="fa-gamepad fa-2x" text="Control Panel" />
               </NavLink>
             </div>
-          )
+      )
         }
       </div>
     );
   }
 }
+
+Topbar.propTypes = {
+  isAdmin: PropTypes.bool.isRequired, // Ensure 'isAdmin' is a required boolean
+};
