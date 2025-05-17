4bf649cbe919efc45e0e0be86a155b2053c21c71
Camillalalala <camilla4clubs@gmail.com>
2025-05-16 18:02:28 -0700

    Included hamburger menu for mobile responsiveness

--- src/components/Topbar/index.jsx
+++ src/components/Topbar/index.jsx
@@ -5,69 +5,93 @@ import NavigationItem from './navigationItem';
 import Config from 'config';
 
 export default class Topbar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { menuOpen: false };
+    this.toggleMenu = this.toggleMenu.bind(this);
+  }
+
+  componentDidMount() {
+    window.addEventListener('resize', this.handleResize);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.handleResize);
+  }
+
+  handleResize = () => {
+    if (window.innerWidth > 768 && this.state.menuOpen) {
+      this.setState({ menuOpen: false });
+    }
+  };
+
+  toggleMenu() {
+    this.setState(prev => ({ menuOpen: !prev.menuOpen }));
+  }
+
   render() {
     const { isAdmin } = this.props;
-    
+    const { menuOpen } = this.state;
+
+    const sharedLinks = (
+      <>
+        <NavLink to="/events" activeClassName="selected">
+          <NavigationItem icon="fa-user" text="Home" />
+        </NavLink>
+        <NavLink to="/events" activeClassName="selected">
+          <NavigationItem icon="fa-calendar" text="Events" />
+        </NavLink>
+        <NavLink to="/leaderboard" activeClassName="selected">
+          <NavigationItem icon="fa-list" text={isAdmin ? 'Members' : 'Leaderboard'} />
+        </NavLink>
+        <NavLink to="/resources" activeClassName="selected">
+          <NavigationItem icon={isAdmin ? 'fa-building' : 'fa-file'} text={isAdmin ? 'Organization' : 'Resources'} />
+        </NavLink>
+      </>
+    );
 
     return (
       <div className="topbar">
-        {
-          /* USER NAVIGATION */ !isAdmin && (
-            <div className="topbar-container">
-              <div className="topbar-logo">
-                <img src={Config.organization.logo} alt={Config.organization.name} />
-              </div>
-              <NavLink to="/events" activeClassName="selected">
-                <NavigationItem icon="fa-user" text="Home" />
-              </NavLink>
-              <NavLink to="/events" activeClassName="selected">
-                <NavigationItem icon="fa-calendar" text="Events" />
-              </NavLink>
-              <NavLink to="/leaderboard" activeClassName="selected">
-                <NavigationItem icon="fa-list" text="Leaderboard" />
-              </NavLink>
-              <NavLink to="/resources" activeClassName="selected">
-                {' '}
-                <NavigationItem icon="fa-file" text="Resources" />
-              </NavLink>
-              <NavLink to="/profile" className="topbar-profile" activeClassName="selected">
-                <img src="/assets/images/unknown.png" alt="Profile" />
-              </NavLink>
-            </div>
-      )
-        }
+        <div className="topbar-container">
+          <div className="topbar-logo">
+            <img src={Config.organization.logo} alt={Config.organization.name} />
+          </div>
 
-        {
-          /* ADMIN NAVIGATION */ isAdmin && (
-            <div className="topbar-container">
-              <div className="topbar-logo">
-                <img src={Config.organization.logo} alt={Config.organization.name} />
-              </div>
-              <NavLink to="/events" activeClassName="selected">
-                <NavigationItem icon="fa-user" text="Home" />
-              </NavLink>
-              <NavLink to="/events" activeClassName="selected">
-                <NavigationItem icon="fa-calendar" text="Events" />
-              </NavLink>
-              <NavLink to="/leaderboard" activeClassName="selected">
-                <NavigationItem icon="fa-list" text="Members" />
-              </NavLink>
-              <NavLink to="/resources" activeClassName="selected">
-                {' '}
-                <NavigationItem icon="fa-building" text="Organization" />
-              </NavLink>
+          <div className={`topbar-links ${menuOpen ? 'open' : ''}`}>
+            {sharedLinks}
+
+            {/* Add Control Panel for Admins */}
+            {isAdmin && (
               <NavLink to="/controlpanel" activeClassName="selected">
-                {' '}
                 <NavigationItem icon="fa-gamepad fa-2x" text="Control Panel" />
               </NavLink>
-            </div>
-      )
-        }
+            )}
+
+            {/* Add Profile Link (Mobile only) */}
+            {!isAdmin && (
+              <NavLink to="/profile" className="topbar-profile-mobile navigation-item" activeClassName="selected">
+                <NavigationItem icon="fa-user-circle" text="Profile" />
+              </NavLink>
+            )}
+          </div>
+
+          {/* Hamburger Button (Mobile only) */}
+          <div className="hamburger" onClick={this.toggleMenu}>
+            <i className="fa fa-bars" />
+          </div>
+
+          {/* Profile Icon (Desktop only) */}
+          {!isAdmin && (
+            <NavLink to="/profile" className="topbar-profile desktop-only" activeClassName="selected">
+              <img src="/assets/images/unknown.png" alt="Profile" />
+            </NavLink> 
+          )}
+        </div>
       </div>
     );
   }
 }
 
 Topbar.propTypes = {
-  isAdmin: PropTypes.bool.isRequired, // Ensure 'isAdmin' is a required boolean
+  isAdmin: PropTypes.bool.isRequired,
 };
