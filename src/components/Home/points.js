c809c07be1b4212f736c936bdd399792ae42d93d
Alex Zheng <75999032+smalex-z@users.noreply.github.com>
2025-05-20 02:06:33 -0700

    Fixed formatting ruined by lint

--- src/components/Home/points.js
+++ src/components/Home/points.js
@@ -8,12 +8,7 @@ export default class Points extends React.Component {
     return (
       <div className="points-component">
         <div className="points-wrapper">
-          <span className="Body-2Black">
-            Level
-            {currLevelNumber + 1}
-            :
-            <b>{currLevel.rank}</b>
-          </span>
+          <span className="Body-2Black">Level {currLevelNumber + 1}: <b>{currLevel.rank}</b></span>
           <span className="num-points Body-2Black">
             {this.props.points}
             {' '}
@@ -21,13 +16,7 @@ export default class Points extends React.Component {
           </span>
           <PointsBar start={currLevel.startsAt} points={this.props.points} end={nextLevel ? nextLevel.startsAt : 0} />
           <span className="Body-3Black">
-            Earn
-            {' '}
-            <b>{ nextLevel ? nextLevel.startsAt - this.props.points : 0 }</b>
-            {' '}
-            more points to reach
-            <b>{ nextLevel ? nextLevel.rank : 'MAX' }</b>
-            !
+            Earn <b>{ nextLevel ? nextLevel.startsAt - this.props.points : 0 }</b> more points to reach <b>{ nextLevel ? nextLevel.rank : 'MAX' }</b>!
           </span>
 
         </div>
