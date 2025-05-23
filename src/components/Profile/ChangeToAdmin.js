0c9e5f9244406904969b50523d0bc1d706884317
Alex Zheng <75999032+smalex-z@users.noreply.github.com>
2025-05-22 23:33:56 -0700

    routes update

--- src/components/Profile/ChangeToAdmin.js
+++ src/components/Profile/ChangeToAdmin.js
@@ -1,6 +1,7 @@
 // src/components/ChangeToAdmin.jsx
 import React, { useState } from 'react';
 import Button from 'components/Button';
+import Config from 'config';
 import './ChangeToAdmin.scss';
 
 const ChangeToAdmin = () => {
@@ -16,7 +17,7 @@ const ChangeToAdmin = () => {
 
   const handleSubmit = async () => {
     try {
-      const response = await fetch('/app/api/v1/admin/promote', {
+      const response = await fetch(Config.API_URL + Config.routes.admin.promote, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
