fb636d44eaf9c1d935da4d4272f2c501db9c0731
Kevin-D23 <116064184+Kevin-D23@users.noreply.github.com>
2025-05-29 00:05:09 -0700

    Added upgrades to event card

--- src/components/Events/UserEvents/eventCard.jsx
+++ src/components/Events/UserEvents/eventCard.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { connect } from 'react-redux';
 import { Action } from '../../../reducers';
+import Config from '../../../config'
 import './style.scss';
 import moment from "moment";
 
@@ -21,9 +22,21 @@ class EventCard extends React.Component {
         super(props);
         this.state = {
             isRsvped: false,
-            loading: false
+            loading: false,
+            isFlipped: false
         };
         this.handleRSVP = this.handleRSVP.bind(this);
+        this.handleFlip = this.handleFlip.bind(this);
+    }
+
+    handleFlip() {
+        this.setState(prev => ({ isFlipped: !prev.isFlipped }));
+    }
+
+    getGoogleMapsUrl(location) {
+        if (!location) return '#';
+        const encodedLocation = encodeURIComponent(location);
+        return `https://www.google.com/maps/search/?api=1&query=${encodedLocation}`;
     }
 
     componentDidMount() {
@@ -72,26 +85,67 @@ class EventCard extends React.Component {
 
     render() {
         const { event } = this.props;
-        const { isRsvped, loading } = this.state;
+        const { isRsvped, loading, isFlipped } = this.state;
+
+        console.log(event.description);
+
+        const committeeColorMap = Object.fromEntries(Config.committeeColors);
+
         return (
             <>
-                <div className="event-container">
-                    <div className="image-container">
-                        <div className="cover" style={{backgroundImage: `url(${event.cover})`}}> </div>
-                        <div className="pill-shape points-container">{event.attendancePoints} PTS</div>
-                    </div>
+                <div className={`event-container ${isFlipped ? 'is-flipped' : ''}`}>
+                    <div className="card-flipper">
+                        {/* Front of card */}
+                        <div className="card-front">
+                            <div className="image-container">
+                                <div 
+                                    className="cover" 
+                                    style={{backgroundImage: `url(${event.cover})`}}
+                                    onClick={this.handleFlip}
+                                > </div>
+                                <div className="pill-shape points-container">{event.attendancePoints} PTS</div>
+                            </div>
 
-                    <div className="text-container">
-                        <p className="event-title">{event.title}</p>
-                        <p className="text">{event.startDate?.format("MMMM D, YYYY")}, {event.startDate?.format("h:mm a")}&mdash;{event.endDate?.format("h:mm a")}</p>
-                        <p className="text">{event.location}</p>
-                        <p className="text">{event.committee}</p>
-                    
-                        <div 
-                            className={`pill-shape rsvp ${isRsvped ? 'rsvped' : ''} ${loading ? 'loading' : ''}`}
-                            onClick={this.handleRSVP}
-                        >
-                            {loading ? 'Loading...' : isRsvped ? 'Cancel RSVP' : 'RSVP'}
+                            <div className="text-container">
+                                <p className="event-title">{event.title}</p>
+                                <p className="text">
+                                    <a 
+                                        href={`https://calendar.google.com/calendar/render?action=TEMPLATE&text=${encodeURIComponent(event.title)}&dates=${event.startDate?.format("YYYYMMDDTHHmmss")}/${event.endDate?.format("YYYYMMDDTHHmmss")}`}
+                                        target="_blank"
+                                        rel="noopener noreferrer"
+                                        className="calendar-link"
+                                    >
+                                        {event.startDate?.format("MMMM D, YYYY")}, {event.startDate?.format("h:mm a")}&mdash;{event.endDate?.format("h:mm a")} 🗓️
+                                    </a>
+                                </p>
+                                <p className="text">
+                                    <a 
+                                        className="location-link"
+                                        href={this.getGoogleMapsUrl(event.location)} 
+                                        target="_blank" 
+                                        rel="noopener noreferrer"
+                                    >
+                                        {event.location} 📍
+                                    </a>
+                                </p>
+                                <p className="text" style={{color: committeeColorMap[event.committee]}}>{event.committee}</p>
+                                <div className={`pill-shape rsvp ${isRsvped ? 'rsvped' : ''} ${loading ? 'loading' : ''}`} onClick={this.handleRSVP}>
+                                    {loading ? 'Loading...' : isRsvped ? 'Cancel RSVP' : 'RSVP'}
+                                </div>
+                            </div>
+                        </div>
+
+                        {/* Back of card */}
+                        <div className="card-back">
+                            <div className="back-content">
+                                <button className="flip-back-btn" onClick={this.handleFlip}>
+                                    <i className="fa fa-arrow-left" /> Back
+                                </button>
+                                <h3>{event.title}</h3>
+                                <div className="description-scroll">
+                                    <p>{event.description}</p>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
