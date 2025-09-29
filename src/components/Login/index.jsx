2c5113087a9052713eb891796430f73966f52c2a
Najm Hoda <najmkhoda@gmail.com>
2025-09-29 15:21:14 -0700

    Fix scroll-locking on leaderboard and events pages

--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -1,15 +1,8 @@
 import React from 'react';
-import Config from 'config';
 import LoginSidebar from './loginSidebar';
 import Banner from './banner';
 
 export default class LoginComponent extends React.Component {
-  componentDidMount() {
-    // Ensure scrolling is disabled on login page
-    document.body.style.overflow = 'hidden';
-    document.documentElement.style.overflow = 'hidden';
-  }
-
   render() {
     return (
       <div className="login">
