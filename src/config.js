10f8449c0ae2a014cd2aa0253cf43e8bb8a1a8b8
smalex-z <alexzheng2004@gmail.com>
2026-02-25 17:14:08 +0000

    remove nginx (handled by caddy on OCI gateway now)

--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@ export default {
   google: {
     apiKey: process.env.GOOGLE_API_KEY,
     authDomain: process.env.GOOGLE_AUTH_DOMAIN,
-    clientId: '913307270840-751ad0psdu1q2a7m81np30g062osgpp5.apps.googleusercontent.com',
+    clientId: process.env.GOOGLE_CLIENT_ID,
     hostedDomain: 'g.ucla.edu',
   },
   routes: {
